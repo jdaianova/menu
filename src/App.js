8bab7226b0bd61c300f0b819906fce6990df5e5c
Julia <jdaianova@gmail.com>
2023-09-14 19:31:06 +0200

    5 commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Menu from './components/Menu/Menu';
 import HomePage from './components/Articles/HomePage';
